1d2d7da5c6e5f893379f5b02bc3847418955689f
Tinku kumar <158788018+tinkumehta@users.noreply.github.com>
2025-02-03 08:55:33 +0530

    readme add question

--- src/Arrays/README.md
+++ src/Arrays/README.md
@@ -7,4 +7,4 @@
 ## Array's easy and medium level question
 <i>This array folder is a lot of practice <i/>
 
-<img src = "https://image3.slideserve.com/5374294/array-l.jpg" width="200px"  />
+<img src = "https://image3.slideserve.com/5374294/array-l.jpg" width="300px"  />
---
+++ src/BinarySearch/LevleOne/Question02.java
@@ -0,0 +1,45 @@
+package BinarySearch.LevleOne;
+
+public class Question02 {
+
+    public static void main(String[] args) {
+        int arr[][] = {{1,3,5,7},{10,11,16,20},{23,30,34,60}};
+        System.out.println(searchMatrix(arr, 3));
+    }
+
+    // search in two 2d matrix
+    static  boolean searchMatrix(int matrix[][], int target)
+    {
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int st=0, end =row * col -1;
+        while (st <= end) {
+            int mid = st +(end - st)/2;
+            int midCR= matrix[mid/col][mid %col];
+            if (midCR == target){
+                return true;
+            }
+             if (target < midCR) {
+                end = mid -1;
+            } else {
+                st = mid +1;
+            }
+        }
+        return false;
+    }
+    // find the peak element
+    static int findPeak(int num[])
+    {
+        int start =0;
+        int end = num.length-1;
+        while (start < end) {
+            int mid = start + ( end -start ) /2;
+            if (num[mid] < num[mid+1]){
+                start = mid +1;
+            } else {
+                end = mid;
+            }
+        }
+        return start;
+    }
+}
