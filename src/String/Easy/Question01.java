52486842addeca17783acdf062d10a10a36de101
Tinku kumar <158788018+tinkumehta@users.noreply.github.com>
2025-02-17 08:20:50 +0530

    add question and file

--- src/String/Easy/Question01.java
+++ src/String/Easy/Question01.java
@@ -3,7 +3,33 @@ import java.util.*;
 
 public class Question01 {
     public static void main(String[] args) {
-        System.out.println(convertToTitle(701));
+       String s =  "A man, a plan, a canal: Panama";
+        //System.out.println(convertToTitle(701));
+        System.out.println(isPalindrome(s));
+    }
+    public static boolean isPalindrome(String s)
+    {
+        if (s.isEmpty()) {
+            return true;
+        }
+        int start =0;
+        int last = s.length()-1;
+        while (start <= last) {
+            char currFirst = s.charAt(start);
+            char currLast = s.charAt(last);
+            if (!Character.isLetterOrDigit(currFirst)){
+                start++;
+            } else if (!Character.isLetterOrDigit(currLast)) {
+                last--;
+            } else {
+                if (Character.toLowerCase(currFirst) != Character.toLowerCase(currLast)){
+                    return false;
+                }
+                start++;
+                last--;
+            }
+        }
+        return true;
     }
     public static String convertToTitle(int columnNumber)
     {
