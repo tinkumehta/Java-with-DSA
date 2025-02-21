6f0a74cdb1752a14ce8bd5ef512b0182bdea9c1c
Tinku kumar <158788018+tinkumehta@users.noreply.github.com>
2025-02-21 09:04:16 +0530

    add question and file

--- src/String/Medium/Question01.java
+++ src/String/Medium/Question01.java
@@ -4,7 +4,20 @@ import java.util.*;
 public class Question01 {
 
     public static void main(String[] args) {
-        System.out.println(calculate("4+3*3"));
+      //  System.out.println(calculate("4+3*3"));
+        String s = "abc";
+        int arr[]= {3, 5,9};
+        System.out.println(shiftingLetters(s, arr));
+    }
+    public static String shiftingLetters(String s, int[] shifts)
+    {
+        char ans[] = s.toCharArray();
+        long sum =0;
+        for(int i= shifts.length-1; i>-1; i--){
+            sum += shifts[i];
+            ans[i] = (char) (((ans[i] - 'a' + sum) % 26) + 'a');
+        }
+        return String.valueOf(ans);
     }
     public static int calculate(String s) {
         Stack<Integer> stack = new Stack<>();
