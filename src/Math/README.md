59ac1f9557eb5404064ffe0ce4b3356d71654bdf
Tinku kumar <158788018+tinkumehta@users.noreply.github.com>
2025-03-16 07:43:17 +0530

    add more question in leetcode

--- src/Math/README.md
+++ src/Math/README.md
@@ -1,1 +1,6 @@
 # Math Function
+
+Java.lang.Math Class methods help to perform numeric operations like square, square root, cube, cube root, exponential and trigonometric operations.
+
+# Methods of Math Class in Java
+Math class consists of methods that can perform mathematical operations and can make long calculations a bit easier. Let us check the method provided in the Math class.
--- src/Math/medium/Question01.java
+++ src/Math/medium/Question01.java
@@ -11,7 +11,8 @@ public class Question01 {
 
         System.out.println(integerBreak(10));
     }
-    public static int integerBreak(int n) {
+    public static int integerBreak(int n)
+    {
         if (n == 2 || n == 3) return (n -1);
         int res =1;
         if (n > 4) {
@@ -36,7 +37,8 @@ public class Question01 {
             }
         return nums;
     }
-    public static int[] countPoints(int[][] points, int[][] queries) {
+    public static int[] countPoints(int[][] points, int[][] queries)
+    {
         int ans [] = new int[queries.length];
         for (int i=0; i<queries.length; i++){
             int x  = queries[i][0];
