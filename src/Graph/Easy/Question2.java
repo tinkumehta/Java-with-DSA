f1b8a7b837d1d8addfb75c488857b518205e3a33
Tinku kumar <158788018+tinkumehta@users.noreply.github.com>
2025-05-14 08:35:11 +0530

    add graph question

--- src/Graph/Easy/Question2.java
+++ src/Graph/Easy/Question2.java
@@ -1,16 +1,42 @@
 package Graph.Easy;
 
 import java.util.ArrayList;
+import java.util.Stack;
 
 public class Question2 {
 
     public static void main(String[] args) {
-        int V = 4;
+        int V = 6;
         ArrayList<Edge> graph [] = new ArrayList[V];
         createGraph(graph);
-        System.out.println(isCycle(graph));
+       // System.out.println(isCycle(graph));
+        topoSort(graph);
     }
+    public static void topoSort(ArrayList<Edge> graph[])
+    {
+        boolean vis[] = new boolean[graph.length];
+        Stack<Integer> s = new Stack<>();
+        for (int i=0; i< graph.length; i++){
+            if (!vis[i]){
+                topoSortUtil(graph, i, vis, s);
+            }
+        }
 
+        while (!s.isEmpty()) {
+            System.out.print(s.pop() + " ");
+        }
+    }
+    public static void topoSortUtil(ArrayList<Edge> graph[], int curr, boolean vis[], Stack<Integer> s)
+    {
+        vis[curr] = true;
+        for (int i=0; i<graph[curr].size(); i++){
+            Edge e = graph[curr].get(i);
+            if (!vis[e.dest]){
+                topoSortUtil(graph, e.dest, vis, s);
+            }
+        }
+        s.push(curr);
+    }
     public static boolean isCycle(ArrayList<Edge> graph[])
     {
         boolean vis[] = new boolean[graph.length];
@@ -70,13 +96,17 @@ public class Question2 {
         for (int i=0; i< graph.length; i++){
             graph[i] = new ArrayList<>();
         }
-       graph[0].add(new Edge(0, 2));
 
-        graph[1].add(new Edge(1, 0));
 
         graph[2].add(new Edge(2, 3));
 
-        graph[3].add(new Edge(3, 0));
+        graph[3].add(new Edge(3, 1));
+
+        graph[4].add(new Edge(4, 0));
+        graph[4].add(new Edge(4, 1));
+
+        graph[5].add(new Edge(5, 0));
+        graph[5].add(new Edge(5, 2));
     }
     static class Edge{
         int src;
