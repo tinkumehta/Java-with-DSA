0a72e0160e5c764808fd3d403a2994f8b3b0132e
Tinku kumar <158788018+tinkumehta@users.noreply.github.com>
2025-05-15 08:49:44 +0530

    add bellman ford algorithm (shortest distance)

--- src/Graph/Easy/Dijkstras.java
+++ src/Graph/Easy/Dijkstras.java
@@ -6,14 +6,52 @@ import java.util.PriorityQueue;
 public class Dijkstras {
 
     public static void main(String[] args) {
-        int V=6;
+        int V=5;
         ArrayList<Edge> graph[] = new ArrayList[V];
         createGraph(graph);
         int src =0;
-        int dist [] = dijkstra(graph, src);
+       bellmanFord(graph, src);
+    }
+    public static void bellmanFord(ArrayList<Edge> graph[], int src)
+    {
+        int dist[] = new int[graph.length];
         for (int i=0; i< dist.length; i++){
+            if (i != src) {
+                dist[i] = Integer.MAX_VALUE;
+            }
+        }
+        // O(V)
+        for (int i=0; i<graph.length-1; i++){
+            // edges - O(E)
+            for (int j =0; j< graph.length; j++){
+                for (int k=0; k<graph[j].size(); k++){
+                    Edge e = graph[j].get(k);
+                    int u = e.src;
+                    int v = e.dest;
+                    int wt = e.wt;
+                    if (dist[u] != Integer.MAX_VALUE && dist[u] + wt < dist[v]){
+                        dist[v] = dist[u] + wt;
+                    }
+                }
+            }
+        }
+        // Detecting negative weight cycle
+        for (int j=0; j<graph.length; j++){
+            for (int k=0; k<graph[j].size(); k++){
+                Edge e = graph[j].get(k);
+                int u = e.src;
+                int v  = e.dest;
+                int wt = e.wt;
+                if (dist[u] != Integer.MAX_VALUE && dist[u] +wt <dist[v]){
+                    System.out.println("negative weight cycle exists");
+                    break;
+                }
+            }
+        }
+        for (int i=0; i<dist.length; i++){
             System.out.print(dist[i] + " ");
         }
+        System.out.println();
     }
     public static int [] dijkstra (ArrayList<Edge> graph[], int src)
     {
@@ -49,18 +87,20 @@ public class Dijkstras {
         for (int i=0; i<graph.length; i++){
             graph[i] = new ArrayList<>();
         }
-        graph[0].add(new Edge(0, 1, 2));
-        graph[0].add(new Edge(0, 2, 4));
+        // [[0, 1, 5], [1, 2, 1], [1, 3, 2], [2, 4, 1], [4, 3, -1]],
+        graph[0].add(new Edge(0, 1, 5));
+      //  graph[0].add(new Edge(0, 2, 4));
 
-        graph[1].add(new Edge(1, 3, 7));
+      //  graph[1].add(new Edge(1, 3, 7));
         graph[1].add(new Edge(1, 2, 1));
+        graph[1].add(new Edge(1, 3, 2));
 
-        graph[2].add(new Edge(2, 4, 3));
+        graph[2].add(new Edge(2, 4, 1));
 
-        graph[3].add(new Edge(3, 5, 1));
+     //   graph[3].add(new Edge(3, 4, 4));
 
-        graph[4].add(new Edge(4, 3, 2));
-        graph[4].add(new Edge(4, 5, 5));
+        graph[4].add(new Edge(4, 3, -1));
+       // graph[4].add(new Edge(4, 5, 5));
     }
     static class Pair implements Comparable<Pair> {
         int n;
