1d0e72d36b5b4454ad0c2b3e16c66f3ce1fc517f
Tinku kumar <158788018+tinkumehta@users.noreply.github.com>
2025-05-30 11:19:41 +0530

    add bfs  algorithm

--- src/Graph/Medium/question01.java
+++ src/Graph/Medium/question01.java
@@ -3,19 +3,42 @@ import  java.util.*;
 
 public class question01 {
     public static void main(String[] args) {
+        int V =5;
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        adj.add(new ArrayList<>(Arrays.asList(1, 2)));
-        adj.add(new ArrayList<>(Arrays.asList(0)));
-        adj.add(new ArrayList<>(Arrays.asList(0)));
-        adj.add(new ArrayList<>(Arrays.asList(4)));
-        adj.add(new ArrayList<>(Arrays.asList(3, 5)));
-        adj.add(new ArrayList<>(Arrays.asList(4)));
-
-        ArrayList<Integer> ans = bfsDisconntecd(adj);
-        for (int i : ans){
-            System.out.print(i + " ");
+       for (int i=0; i<V; i++){
+           adj.add(new ArrayList<>());
+       }
+        int[][] edges= { { 1, 2 },{ 1, 0 },{ 2, 0 },{ 2, 3 },{ 2, 4 } };
+       for (int edge[] : edges){
+           addEdge(adj, edge[0], edge[1]);
+       }
+       ArrayList<Integer> res = DFS(adj);
+       for (int i=0; i<res.size(); i++){
+           System.out.print(res.get(i) + " ");
+       }
+    }
+    public static void addEdge(ArrayList<ArrayList<Integer> > adj, int s, int t)
+    {
+        adj.get(s).add(t);
+        adj.get(t).add(s);
+    }
+    public static ArrayList<Integer> DFS(ArrayList<ArrayList<Integer> > adj)
+    {
+        boolean vis[] = new boolean[adj.size()];
+        ArrayList<Integer> res = new ArrayList<>();
+        dfsRec(adj, vis, 0, res);
+        return res;
+    }
+    private static void dfsRec(ArrayList<ArrayList<Integer> > adj, boolean vis[],
+                                int s,  ArrayList<Integer> res)
+    {
+        vis[s] = true;
+        res.add(s);
+        for (int i : adj.get(s)){
+            if (!vis[i]){
+                dfsRec(adj, vis, i, res);
+            }
         }
-        System.out.println();
     }
     static ArrayList<Integer> bfsDisconntecd(ArrayList<ArrayList<Integer>> adj)
     {
