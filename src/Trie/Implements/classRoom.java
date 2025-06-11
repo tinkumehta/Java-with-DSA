2e01fe19a03f10c20dfdf8cb88a1612010695655
Tinku kumar <158788018+tinkumehta@users.noreply.github.com>
2025-06-11 08:41:00 +0530

    add Trie data structer

--- src/Trie/Implements/classRoom.java
+++ src/Trie/Implements/classRoom.java
@@ -13,15 +13,43 @@ public class classRoom {
         }
     }
     static Node root = new Node();
+    public static String ans ="";
 
     public static void main(String[] args) {
-        String words [] = {"apple", "app", "mango", "man", "women"};
+        String words [] = {"a", "banana", "app", "appl", "ap", "apply", "apple"};
         String key = "moon";
         for (int i=0; i<words.length; i++){
             insert(words[i]);
         }
-        //System.out.println(wordBreak(key));
-        System.out.println(startWith(key));
+      //  System.out.println(wordBreak(key));
+        //System.out.println(startWith(key));
+        longestWord(root, new StringBuilder(""));
+        System.out.println(ans);
+    }
+    public static void longestWord(Node root, StringBuilder temp)
+    {
+        if (root == null)return;
+
+        for (int i=0; i<26; i++){
+            if (root.children[i] != null && root.children[i].eow == true){
+                temp.append((char) (i + 'a'));
+                if (temp.length() > ans.length()) {
+                    ans = temp.toString();
+                }
+                longestWord(root.children[i], temp);
+                temp.deleteCharAt(temp.length() -1);
+            }
+        }
+    }
+    static int countNode(Node root)
+    {
+        int count =0;
+        for (int i=0; i<26; i++){
+            if (root.children[i] != null){
+                count += countNode(root.children[i]);
+            }
+        }
+        return count+1;
     }
     public static boolean startWith(String perfix)
     {
