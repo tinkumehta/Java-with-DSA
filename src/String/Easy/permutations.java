b6fd788b84ee5b48728a38e04661517d50be4722
Tinku kumar <158788018+tinkumehta@users.noreply.github.com>
2025-11-05 16:02:39 +0530

    add leetcode problem

--- src/String/Easy/permutations.java
+++ src/String/Easy/permutations.java
@@ -11,6 +11,21 @@ public class permutations {
      //   pad("", "12");
      //   System.out.println(padRet("", "12"));
         dice("", 4);
+        ArrayList<String> list = diceRet("", 4);
+        System.out.println(list);
+    }
+    static ArrayList<String> diceRet(String p, int target)
+    {
+        if (target ==0){
+            ArrayList<String>list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        ArrayList<String> list = new ArrayList<>();
+        for (int i=1; i<=6 && i<=target; i++){
+            list.addAll(diceRet(p+i, target-i));
+        }
+        return list;
     }
     static void dice(String p,int target)
     {
